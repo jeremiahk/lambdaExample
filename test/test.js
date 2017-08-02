c96802901bc9b29427b94663ab637d1a732e1032
Jenkins <jenkins@AMCOEs-Mac-Pro.local>
2017-08-02 09:19:44 -0500

    Add basic test.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expectd = require()
+
+describe("Index Tests", function() {
+    describe("hello world tests", function() {
+        it('The index function should have worked', function(done) {
+
+        })
+    });
+});
