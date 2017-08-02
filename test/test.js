a2090be5c7312fd68be4ae70c3ee19ca571fe4f5
Jeremiah Kistler <jeremiah.kistler@unisys.com>
2017-08-02 11:51:05 -0500

    Add passing and failing test.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,15 @@
-var expectd = require()
+var expect = require("chai").expect;
 
 describe("Index Tests", function() {
-    describe("hello world tests", function() {
-        it('The index function should have worked', function(done) {
-
-        })
+  describe("hello world tests", function() {
+    it('The index function should have worked', function(done) {
+      expect(true).to.be.true;
+      done();
     });
+
+    it('The index function should not have worked', function(done) {
+      expect(false).to.be.true;
+      done();
+    });
+  });
 });
